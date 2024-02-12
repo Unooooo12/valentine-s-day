6687cc9d3c913ae49ff51edf7f1807e16890007a
Unooooo12 <159711803+Unooooo12@users.noreply.github.com>
2024-02-12 09:27:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const Wrapper = document.querySelector('.Wrapper');
+const Question = document.querySelector('.Question');
+const gif= document.querySelector('.gif');
+const YesBtn = document.querySelector('.Yes-btn');
+const NoBtn = document.querySelector('.No-btn');
+
+YesBtn.addEventListener("click", ()=> {
+    Question.innerHTML = "I'll see you on February 14!!";
+    gif.src = "https://media1.tenor.com/images/10b9c449d8068481470f48451dc7789a/tenor.gif?itemid=12395952";
+});
+
+NoBtn.addEventListener("mouseover", ()=>{
+const NoBtnRect = NoBtn.getBoundingClientRect();
+const maxX = window.innerWidth -NoBtnRect.width;
+const maxY = window.innerHeight - NoBtnRect.height;
+
+const randomX = Math.floor(Math.random() * maxX );
+const randomY = Math.floor(Math.random()* maxY );
+
+NoBtn.style.left = randomX + "px";
+NoBtn.style.top = randomY + "px";
+});
